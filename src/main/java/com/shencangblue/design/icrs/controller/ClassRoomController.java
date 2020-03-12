5f7b0b93cbbd9ad341dd0a3f38d5ba8683dcd475
shencang <2630610009@qq.com>
2020-03-12 23:07:13 +0800

    ClassRoom-api

--- src/main/java/com/shencangblue/design/icrs/controller/ClassRoomController.java
+++ src/main/java/com/shencangblue/design/icrs/controller/ClassRoomController.java
@@ -3,41 +3,51 @@ package com.shencangblue.design.icrs.controller;
 import com.shencangblue.design.icrs.dao.ClassRoomDao;
 import com.shencangblue.design.icrs.model.ClassRoom;
 import com.shencangblue.design.icrs.service.ClassRoomService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
 @RestController
-@RequestMapping("/class")
+@RequestMapping("/api")
 public class ClassRoomController {
     @Resource
     private ClassRoomService classRoomService;
 
-    @RequestMapping("/save")
-    public String save(){
-        ClassRoom classRoom = new ClassRoom();
-        classRoom.setRoomName("测试");
-        classRoom.setDescription("test");
-        classRoom.setCapacity(20);
-        classRoom.setStatus(1);
+    @CrossOrigin
+    @RequestMapping("/rooms/save")
+    public ClassRoom save(@RequestBody ClassRoom classRoom){
         classRoomService.save(classRoom);
-        return "数据添加成功!";
+        return classRoom;
     }
-    @RequestMapping("/delete")
-    public String delete(){
-        classRoomService.delete(2L);
-        return  "删除成功!";
+    @CrossOrigin
+    @RequestMapping("/rooms/delete")
+    public ClassRoom delete(@RequestBody ClassRoom classRoom){
+        System.out.println(classRoom.getRoomId());
+        classRoomService.delete(classRoom.getRoomId());
+        return classRoom;
     }
-
-    @RequestMapping("/all")
+    @CrossOrigin
+    @RequestMapping("/rooms")
     public Iterable<ClassRoom> getAll(){
         return classRoomService.getAll();
     }
 
-    @RequestMapping("/update")
+    @CrossOrigin
+    @RequestMapping("/rooms/update")
     public String update(){
         return classRoomService.update(3L,classRoomService.getById(3L))!=null?"sec":"fal";
+    }
+
+
+    @CrossOrigin
+    @RequestMapping("/rooms/capacity/{capacity}/rooms")
+    public Iterable<ClassRoom> findAllByCapacity(@PathVariable("capacity") int capacity){
+        System.out.println(capacity);
+        if (0==capacity){
+            return classRoomService.getAll();
+        }else {
+            return classRoomService.findAllByCapacity(capacity);
+        }
     }
 
 }
