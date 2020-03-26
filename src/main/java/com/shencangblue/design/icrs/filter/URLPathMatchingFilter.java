38045a05ee5214eb47274236316d0db855e17c46
shencang <2630610009@qq.com>
2020-03-26 23:56:42 +0800

    filter

---
+++ src/main/java/com/shencangblue/design/icrs/filter/URLPathMatchingFilter.java
@@ -0,0 +1,73 @@
+package com.shencangblue.design.icrs.filter;
+
+import com.shencangblue.design.icrs.service.admin.AdminPermissionService;
+import com.shencangblue.design.icrs.utils.SpringContextUtils;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.web.filter.PathMatchingFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+
+import javax.annotation.Resource;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Set;
+
+public class URLPathMatchingFilter extends PathMatchingFilter {
+    @Resource
+    AdminPermissionService adminPermissionService;
+
+    @Override
+    protected boolean onPreHandle(ServletRequest request, ServletResponse response, Object mappedValue) throws Exception {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+
+        if (HttpMethod.OPTIONS.toString().equals((httpServletRequest).getMethod())) {
+            httpServletResponse.setStatus(HttpStatus.NO_CONTENT.value());
+            return true;
+        }
+
+        if (null==adminPermissionService) {
+            adminPermissionService = SpringContextUtils.getContext().getBean(AdminPermissionService.class);
+        }
+
+        String requestAPI = getPathWithinApplication(request);
+
+        Subject subject = SecurityUtils.getSubject();
+
+        if (!subject.isAuthenticated()) {
+            System.out.println("需要登录");
+            return false;
+        }
+
+        // 判断访问接口是否需要过滤（数据库中是否有对应信息）
+        boolean needFilter = adminPermissionService.needFilter(requestAPI);
+        if (!needFilter) {
+            System.out.println("接口：" + requestAPI + "无需权限");
+            return true;
+        } else {
+            // 判断当前用户是否有相应权限
+            boolean hasPermission = false;
+            String username = subject.getPrincipal().toString();
+            Set<String> permissionAPIs = adminPermissionService.listPermissionURLsByUser(username);
+            for (String api : permissionAPIs) {
+                // 匹配前缀
+                if (requestAPI.startsWith(api)) {
+                    hasPermission = true;
+                    break;
+                }
+            }
+
+            if (hasPermission) {
+                System.out.println("访问权限：" + requestAPI + "验证成功");
+                return true;
+            } else {
+                System.out.println("当前用户没有访问接口" + requestAPI + "的权限");
+                return false;
+            }
+        }
+    }
+}
